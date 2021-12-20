f5a1e6cc31fa5a85109793a0cd47f290fc7e49b1
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-12-20 13:40:07 +0530

    Add more unit tests for the Executor class methods

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ class Executor extends EventEmitter {
      * @param name Task's name
      * @returns Executor already has the task registered
      */
-    hasTask = (name: string) => {
+    hasTask = (name: string): boolean => {
         return name in this._tasks
     }
 
@@ -43,7 +43,7 @@ class Executor extends EventEmitter {
      * Retrieve the task object
      * @param name Task's name
      */
-    getTask = (name: string) => {
+    getTask = (name: string): task | null => {
         return this.hasTask(name) ? this._tasks[name] : null
     }
 
@@ -52,13 +52,61 @@ class Executor extends EventEmitter {
      * @param name Task's name
      */
     removeTask = (name: string) => {
-        if (!this.hasTask(name)) { return }
+        if (!this.hasTask(name)) { return this }
         delete this._tasks[name]
+        return this
+    }
+
+    private _runTask = (sequence: string[], taskNumber: number) => {
+
+        //  Exit if tasks completed
+        if (taskNumber >= sequence.length) {
+            this._isRunning = false
+            return this.emit('finish')
+        }
+
+        const task = this.getTask(sequence[taskNumber])
+
+        //  If task does not exist then return
+        if (!task) { return }   //? Maybe add additional error handling / logs for this
+
+        this.emit('task::start', task.name)
+        task.callback()
+        task.done = true
+        this.emit('task::done', task.name)
+
+        return process.nextTick(() => this._runTask(sequence, taskNumber + 1))
+    }
+
+    execute = (...taskNames: string[]) => {
+        if (this._isRunning) { return }     //  Short-circuit if already running
+
+        this._isRunning = true
+        const tasks: string[] = []
+
+        //  Collect all tasks
+        if (!taskNames || taskNames.length <= 0) {  //  If no arguments were passed in, run all tasks
+            for (const task in this._tasks) {
+                tasks.push(task)
+            }
+        } else {    //  Collect only specified tasks
+            taskNames.forEach(task => tasks.push(task))
+        }
+
+
+        //TODO: Build taskQueue based on dependencies
+
+        //  Start execution process
+        this.emit('start')
+
+        //  Initialize the task queue in the next tick. This ensures event listeners are registered before execution
+        process.nextTick(() => this._runTask(tasks, 0))
+
         return this
     }
 
 }
 
-//---------------------
+//  -------------------
 export default Executor
-//---------------------
+//  -------------------
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,7 +5,105 @@ import * as assert from 'assert'
 //  Library
 import Executor from '../src'
 
-test('Should create an instance of Executor', () => {
+//  CONSTRUCTOR
+//  ===========
+
+test('[addTask] Should create an instance of Executor', () => {
     const executor = new Executor()
-    assert.equal(executor instanceof Executor, true, 'Not an instance of Executor')
+    assert.equal(executor instanceof Executor, true)
+})
+
+//  ADD TASK
+//  ========
+
+test('[addTask] Should create a new task', () => {
+    const executor = new Executor()
+    const taskName = 'Task1'
+    executor.addTask(taskName, () => console.log(`Executing ${taskName}`))
+    assert.equal(executor.getTask(taskName)?.name, taskName)
+})
+
+test('[addTask] Should execute the callback function properly', () => {
+    const executor = new Executor()
+    const callback = () => 'Executing Task 2'
+    executor.addTask('Task2', callback)
+    assert.equal(executor.getTask('Task2')?.callback(), callback())
+})
+
+test('[addTask] Should create the dependencies array', () => {
+    const executor = new Executor()
+    const taskName = 'Task3'
+    const dependencies = ['Task1', 'Task2']
+    executor.addTask(taskName, () => console.log(`Executing ${taskName}`), dependencies)
+    assert.deepEqual(executor.getTask(taskName)?.dependencies, dependencies)
+})
+
+test('[addTask] Should initialize the done parameter as false', () => {
+    const executor = new Executor()
+    const taskName = 'Task4'
+    executor.addTask(taskName, () => console.log(`Executing ${taskName}`))
+    assert.equal(executor.getTask(taskName)?.done, false)
+})
+
+test('[addTask] Should return self', () => {
+    const executor = new Executor()
+    const self = executor.addTask('Task5', () => 'Executing Task5')
+    assert.equal(self instanceof Executor, true)
+})
+
+//  HAS TASK
+//  ========
+
+test('[hasTask] Should return true if task exists', () => {
+    const executor = new Executor()
+    const taskName = 'Task6'
+    executor.addTask(taskName, () => console.log(`Executing ${taskName}`))
+    assert.equal(executor.hasTask(taskName), true)
+})
+
+test('[hasTask] Should return false if task does not exist', () => {
+    const executor = new Executor()
+    const taskName = 'YOLO'
+    assert.equal(executor.hasTask(taskName), false)
+})
+
+//  GET TASK
+//  ========
+
+test('[getTask] Should return a task if it exists', () => {
+    const executor = new Executor()
+    const taskName = 'Task7'
+    const dependencies = ['Task3', 'Task5']
+    executor.addTask(taskName, () => console.log(`Executing ${taskName}`), dependencies)
+    const task = executor.getTask(taskName)
+    assert.equal(task?.name, taskName)
+    assert.equal(task?.dependencies, dependencies)
+    assert.equal(task?.done, false)
+})
+
+test('[getTask] Should return null if a task does not exist', () => {
+    const executor = new Executor()
+    const taskName = 'YOLO'
+    assert.equal(executor.getTask(taskName), null)
+})
+
+//  REMOVE TASK
+//  ===========
+
+test('[removeTask] Should return self', () => {
+    const executor = new Executor()
+    let self = executor.removeTask('YOLO')
+    assert.equal(self instanceof Executor, true)
+    executor.addTask('Task8', () => console.log('Executing Task8'))
+    self = executor.removeTask('Task8')
+    assert.equal(self instanceof Executor, true)
+})
+
+test('[removeTask] Should remove the given task from the list', () => {
+    const executor = new Executor()
+    const taskName = 'Task9'
+    executor.addTask(taskName, () => console.log(`Executing ${taskName}`))
+    assert.equal(executor.getTask(taskName)?.name, taskName, 'Failed to create task')
+    executor.removeTask(taskName)
+    assert.equal(executor.getTask(taskName), null)
 })
